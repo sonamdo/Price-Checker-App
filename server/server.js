df464f13831d7a16e06abf5bf9bd605aca05528a
Sonam <sonamklein@gmail.com>
2018-08-14 21:31:39 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+  console.log('Server is up on port ', port)
 });
