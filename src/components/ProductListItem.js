b6ba684e7e5cf3c33f6b8849b6b254e7b437c563
Sonam <sonamklein@gmail.com>
2018-09-09 20:59:01 -0400

    added up to date products from webscraper and configured links

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ProductListItem = ({website, model, brand, price}) => (//takes product info from data array and renders into jsx
+const ProductListItem = ({website, model, brand, price, link}) => (//takes product info from data array and renders into jsx
   <tr className = "product">
-    <td>{model}</td>
-    <td>{price}</td>
+    <td>
+      <a href = {link}>
+        {model}
+      </a>
+    </td>
+    <td>${price}</td>
     <td>{brand}</td>
     <td>{website}</td>
   </tr>
